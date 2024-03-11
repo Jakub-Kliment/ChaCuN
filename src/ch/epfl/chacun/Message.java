7c0f3b7058278a38ac686247fae6f912dc15b65c
Jakub Kliment <jakub.kliment@epfl.ch>
2024-03-11 21:47:32 +0100

    etape 4 3.2 commencee

---
+++ src/ch/epfl/chacun/Message.java
@@ -0,0 +1,67 @@
+package ch.epfl.chacun;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+
+public record Message(TextMaker textMaker, List<Message> messages) {
+
+    public Message {
+        // Besoin immuabilite !!!!!!!
+        messages = List.copyOf(messages);
+    }
+
+    public Map<PlayerColor, Integer> points() {
+        return null;
+    }
+
+    public MessageBoard withScoredForest(Area<Zone.Forest> forest) {
+        return null;
+    }
+
+    public MessageBoard withClosedForestWithMenhir(PlayerColor player, Area<Zone.Forest> forest) {
+        return null;
+    }
+
+    public MessageBoard withScoredRiver(Area<Zone.River> river) {
+        return null;
+    }
+
+    public MessageBoard withScoredHuntingTrap(PlayerColor scorer, Area<Zone.Meadow> adjacentMeadow) {
+        return null;
+    }
+
+    public MessageBoard withScoredLogboat(PlayerColor scorer, Area<Zone.Water> riverSystem) {
+        return null;
+    }
+
+    public MessageBoard withScoredMeadow(Area<Zone.Meadow> meadow, Set<Animal> cancelledAnimals) {
+        return null;
+    }
+
+    public MessageBoard withScoredRiverSystem(Area<Zone.Water> riverSystem) {
+        return null;
+    }
+
+    public MessageBoard withScoredPitTrap(Area<Zone.Meadow> adjacentMeadow, Set<Animal> cancelledAnimals) {
+        return null;
+    }
+
+    public MessageBoard withScoredRaft(Area<Zone.Water> riverSystem) {
+        return null;
+    }
+
+    public MessageBoard withWinners(Set<PlayerColor> winners, int points) {
+        return null;
+    }
+
+    public record MessageBoard(String text, int points, Set<PlayerColor> scorers, Set<Integer> tileIds) {
+        public MessageBoard {
+            Objects.requireNonNull(text);
+            Preconditions.checkArgument(points >= 0);
+            scorers = Set.copyOf(scorers);
+            tileIds = Set.copyOf(tileIds);
+        }
+    }
+ }
