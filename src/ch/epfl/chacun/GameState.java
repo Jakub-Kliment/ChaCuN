dabd4120bf30877d4586e93ee9cff7075455461a
Jakub Kliment <jakub.kliment@epfl.ch>
2024-03-24 10:31:17 +0100

    etape 6

--- src/ch/epfl/chacun/GameState.java
+++ src/ch/epfl/chacun/GameState.java
@@ -1,60 +1,20 @@
 package ch.epfl.chacun;
 
-import ch.epfl.chacun.tile.Tiles;
+import java.util.*;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+public record GameState(List<PlayerColor> players, TileDecks tileDecks, Tile tileToPlace,
+                        Board board, Action nextAction, MessageBoard messageBoard) {
 
-public record GameState(List<PlayerColor> players,
-                        TileDecks tileDecks,
-                        Tile tileToPlace,
-                        Board board,
-                        Action nextAction,
-                        MessageBoard messageBoard) {
-
-    public GameState{
+    public GameState {
         //Peut-être être truc à faire pour être immuable
         Preconditions.checkArgument(players.size() >= 2);
         players = List.copyOf(players);
-        Preconditions.checkArgument((tileToPlace == null) ^ (nextAction == Action.PLACE_TILE));
-        Preconditions.checkArgument((tileDecks != null) && (board != null) && (nextAction != null) && (messageBoard != null));
-
-
-    }
-
-    public static GameState initial(List<PlayerColor> players, TileDecks tileDecks, TextMaker textMaker){
-        return new GameState(players, tileDecks, null, Board.EMPTY, Action.PLACE_TILE, new MessageBoard(textMaker, new ArrayList<>()));
-    }
-
-    public PlayerColor currentPlayer(){
-        if (nextAction == Action.START_GAME || nextAction == Action.END_GAME){
-            return null;
-        }
-        return players.getFirst();
-    }
-
-    public int freeOccupantsCount(PlayerColor player, Occupant.Kind kind){
-        return Occupant.occupantsCount(kind) - board.occupantCount(player, kind);
-    }
-
-    public Set<Occupant> lastTilePotentialOccupants(){
-        Preconditions.checkArgument(board == Board.EMPTY);
-        return board.lastPlacedTile().potentialOccupants();
-    }
-
-    public GameState withStartingTilePlaced(){
-        Preconditions.checkArgument(nextAction == Action.PLACE_TILE);
-        Board nextBoard = board.withNewTile(new PlacedTile(tileDecks.topTile(Tile.Kind.START), null, Rotation.NONE, new Pos(0,0)));
-        Tile nextTile = tileDecks.topTile(Tile.Kind.NORMAL);
-        TileDecks nextDeck = tileDecks.withTopTileDrawn(Tile.Kind.START).withTopTileDrawn(Tile.Kind.NORMAL);
-        return new GameState(players, nextDeck, nextTile, nextBoard, nextAction, messageBoard);
-    }
-
-    public GameState withPlacedTile(PlacedTile tile){
-        Preconditions.checkArgument(nextAction == Action.PLACE_TILE && tile.occupant() != null);
-        Board nextBoard = board.withNewTile(tile);
-
+        // Demander si cest un xor ou or !!!!!!
+        Preconditions.checkArgument((tileToPlace == null) ^ nextAction.equals(Action.PLACE_TILE));
+        Objects.requireNonNull(tileDecks);
+        Objects.requireNonNull(board);
+        Objects.requireNonNull(nextAction);
+        Objects.requireNonNull(messageBoard);
     }
 
     public enum Action {
@@ -65,5 +25,38 @@ public record GameState(List<PlayerColor> players,
         END_GAME
     }
 
+    public static GameState initial(List<PlayerColor> players, TileDecks tileDecks, TextMaker textMaker) {
+        return new GameState(players, tileDecks, null, Board.EMPTY,
+                Action.START_GAME, new MessageBoard(textMaker, new ArrayList<>()));
+    }
 
+    public PlayerColor currentPlayer() {
+        if (nextAction.equals(Action.START_GAME) || nextAction.equals(Action.END_GAME))
+            return null;
+        return players.getFirst();
+    }
+
+    public int freeOccupantsCount(PlayerColor player, Occupant.Kind kind) {
+        return Occupant.occupantsCount(kind) - board.occupantCount(player, kind);
+    }
+
+    public Set<Occupant> lastTilePotentialOccupants() {
+        Preconditions.checkArgument(board.equals(Board.EMPTY));
+        return board.lastPlacedTile().potentialOccupants();
+    }
+
+    public GameState withStartingTilePlaced() {
+        Preconditions.checkArgument(nextAction.equals(Action.START_GAME));
+        Board newBoard = board.withNewTile(new PlacedTile(tileDecks.topTile(Tile.Kind.START),
+                null, Rotation.NONE, Pos.ORIGIN));
+        Tile nextTile = tileDecks.topTile(Tile.Kind.NORMAL);
+        TileDecks nextDecks = tileDecks.withTopTileDrawn(Tile.Kind.START).withTopTileDrawn(Tile.Kind.NORMAL);
+        return new GameState(players, nextDecks, nextTile, newBoard, Action.PLACE_TILE, messageBoard);
+    }
+
+    public GameState withPlacedTile(PlacedTile tile) {
+        Preconditions.checkArgument(nextAction.equals(Action.PLACE_TILE) && tile.occupant() != null);
+        Board newBoard = board.withNewTile(tile);
+        return null;
+    }
 }
