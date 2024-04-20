0d241adbc3337726dc8a2d417cdd63276e6c5b45
Alexis Théo André Grillet-Aubert <alexis.grillet-aubert@epfl.ch>
2024-04-20 10:15:08 +0200

    MessageBoardUI pas fini

---
+++ src/ch/epfl/chacun/gui/MessageBoardUI.java
@@ -0,0 +1,40 @@
+package ch.epfl.chacun.gui;
+
+import ch.epfl.chacun.MessageBoard;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.Node;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
+import java.util.List;
+import java.util.Set;
+
+import static ch.epfl.chacun.gui.ImageLoader.*;
+import static javafx.application.Platform.runLater;
+
+public class MessageBoardUI {
+    private MessageBoardUI(){}
+
+    public static Node create(ObservableValue<List<MessageBoard.Message>> listMessage, ObjectProperty<Set<Integer>> tileId){
+        ScrollPane messagePane = new ScrollPane();
+        messagePane.getStylesheets().add("message-board.css");
+
+
+        VBox box = new VBox();
+        messagePane.setContent(box);
+
+        listMessage.addListener((l, old, next) -> {
+            for (int i = next.size() - old.size(); i > 0 ; i--){
+                Text message = new Text(next.get(next.size()-i).text());
+                message.setWrappingWidth(LARGE_TILE_FIT_SIZE);
+                box.getChildren().add(message);
+            }
+        });
+
+        runLater(() -> messagePane.setVvalue(1));
+
+        return messagePane;
+    }
+}
