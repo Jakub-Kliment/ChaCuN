206d9973573fd3dfbc201ab4a045d1ddf66edd99
Alexis Théo André Grillet-Aubert <alexis.grillet-aubert@epfl.ch>
2024-04-23 00:21:15 +0200

    Tout fini, test mtn

---
+++ src/ch/epfl/chacun/gui/DecksUI.java
@@ -0,0 +1,98 @@
+package ch.epfl.chacun.gui;
+
+import ch.epfl.chacun.Board;
+import ch.epfl.chacun.Occupant;
+import ch.epfl.chacun.Tile;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.Node;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
+import java.util.function.Consumer;
+
+public class DecksUI {
+    private DecksUI(){}
+
+    public static Node create(ObservableValue<Tile> tileToPlace, ObservableValue<Integer> normalTile,
+                              ObservableValue<Integer> menhirTile, ObservableValue<String> text, Consumer<Occupant> event){
+
+        VBox box = new VBox();
+        box.getStylesheets().add("decks.css");
+
+
+
+
+
+        StackPane currentTile = new StackPane();
+        currentTile.getStyleClass().add("next-tile");
+        box.getChildren().add(currentTile);
+
+        Text textCurrent = new Text();
+        textCurrent.visibleProperty().bind(text.map(s -> !s.isEmpty()));
+        textCurrent.setOnMouseClicked(e -> event.accept(null));
+        textCurrent.setWrappingWidth(0.8*ImageLoader.LARGE_TILE_FIT_SIZE);
+        currentTile.getChildren().add(textCurrent);
+
+
+        ImageView imageCurrent = new ImageView();
+        ObservableValue<Image> image = tileToPlace.map(t -> ImageLoader.largeImageForTil(t.id()));
+        image.addListener((i, old, next) -> {
+            imageCurrent.setImage(next);
+        });
+        //TODO 2 ligne d'apres inutile je pense a verifier
+        imageCurrent.setFitHeight(ImageLoader.LARGE_TILE_PIXEL_SIZE);
+        imageCurrent.setFitWidth(ImageLoader.LARGE_TILE_PIXEL_SIZE);
+        imageCurrent.visibleProperty().bind(text.map(String::isEmpty));
+        currentTile.getChildren().add(imageCurrent);
+
+
+
+
+
+
+        HBox hBoxDecks = new HBox();
+        hBoxDecks.getStyleClass().add("decks");
+        box.getChildren().add(hBoxDecks);
+
+        StackPane tileMenhir = new StackPane();
+        hBoxDecks.getChildren().add(tileMenhir);
+
+        ImageView imageMenhir = new ImageView(new Image("/512/MENHIR.jpg"));
+        //TODO Demande pk cette ligne doit etre là, est ce qu'elle difinit l'image ?
+        imageMenhir.getStyleClass().add("MENHIR");
+        imageMenhir.setFitWidth(ImageLoader.LARGE_TILE_FIT_SIZE);
+        imageMenhir.setFitHeight(ImageLoader.LARGE_TILE_FIT_SIZE);
+        tileMenhir.getChildren().add(imageMenhir);
+
+        Text textMenhir = new Text();
+        textMenhir.textProperty().bind(menhirTile.map(i -> STR."\{i}"));
+        tileMenhir.getChildren().add(textMenhir);
+
+
+
+
+
+
+
+        StackPane tileNormal = new StackPane();
+        hBoxDecks.getChildren().add(tileNormal);
+
+        ImageView imageNormal = new ImageView(new Image("/512/NORMAL.jpg"));
+        //TODO Demande pk cette ligne doit etre là, est ce qu'elle difinit l'image ?
+        imageNormal.getStyleClass().add("NORMAL");
+        imageNormal.setFitWidth(ImageLoader.LARGE_TILE_FIT_SIZE);
+        imageNormal.setFitHeight(ImageLoader.LARGE_TILE_FIT_SIZE);
+        tileNormal.getChildren().add(imageNormal);
+
+        Text textNormal = new Text();
+        textNormal.textProperty().bind(normalTile.map(i -> STR."\{i}"));
+        tileNormal.getChildren().add(textNormal);
+
+        return box;
+    }
+}
