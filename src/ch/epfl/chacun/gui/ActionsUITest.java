a73282236db1ebaf33edef2bb4c2d3f57ee68a62
Jakub Kliment <jakub.kliment@epfl.ch>
2024-05-09 17:46:40 +0200

    actions test

--- src/ch/epfl/chacun/gui/ActionsUITest.java
+++ src/ch/epfl/chacun/gui/ActionsUITest.java
@@ -24,33 +24,14 @@ public class ActionsUITest extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        var playerNames = Map.of(PlayerColor.RED, "Rose",
-                PlayerColor.BLUE, "Bernard");
-        var playerColors = playerNames.keySet().stream()
-                .sorted()
-                .toList();
-
-        var tilesByKind = Tiles.TILES.stream()
-                .collect(Collectors.groupingBy(Tile::kind));
-        var tileDecks =
-                new TileDecks(tilesByKind.get(Tile.Kind.START),
-                        tilesByKind.get(Tile.Kind.NORMAL),
-                        tilesByKind.get(Tile.Kind.MENHIR));
-
-        var textMaker = new TextMakerFr(playerNames);
-
-        var gameState =
-                GameState.initial(playerColors,
-                        tileDecks,
-                        textMaker);
-
-        var gameStateO = new SimpleObjectProperty<>(gameState);
 
         SimpleObjectProperty<List<String>> messages = new SimpleObjectProperty<>(List.of());
         var actionsNode = ActionsUI.create(messages, s -> System.out.println(STR."String :\{s}"));
         var root2Node = new ScrollPane(actionsNode);
         primaryStage.setScene(new Scene(root2Node));
+
         primaryStage.setTitle("ChaCuN test");
         primaryStage.show();
+
     }
 }
