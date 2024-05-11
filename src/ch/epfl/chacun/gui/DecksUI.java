3e101c7c9fdf372156fd1c903c276db8261f6069
Jakub Kliment <jakub.kliment@epfl.ch>
2024-05-12 00:35:54 +0200

    decks styles

--- src/ch/epfl/chacun/gui/DecksUI.java
+++ src/ch/epfl/chacun/gui/DecksUI.java
@@ -26,10 +26,8 @@ public class DecksUI {
 
         VBox box = new VBox();
         box.getStylesheets().add("decks.css");
-        box.setId("decks");
 
         HBox hBoxDecks = new HBox();
-        hBoxDecks.getStyleClass().add("decks");
         hBoxDecks.setId("decks");
         box.getChildren().add(hBoxDecks);
 
@@ -37,8 +35,7 @@ public class DecksUI {
         StackPane tileNormal = new StackPane();
         hBoxDecks.getChildren().add(tileNormal);
 
-        ImageView imageNormal = new ImageView(new Image("/512/NORMAL.jpg"));
-        imageNormal.getStyleClass().add("NORMAL");
+        ImageView imageNormal = new ImageView(new Image("/256/NORMAL.jpg"));
         imageNormal.setId("NORMAL");
         imageNormal.setFitWidth(ImageLoader.NORMAL_TILE_FIT_SIZE);
         imageNormal.setFitHeight(ImageLoader.NORMAL_TILE_FIT_SIZE);
@@ -52,8 +49,7 @@ public class DecksUI {
         StackPane tileMenhir = new StackPane();
         hBoxDecks.getChildren().add(tileMenhir);
 
-        ImageView imageMenhir = new ImageView(new Image("/512/MENHIR.jpg"));
-        imageMenhir.getStyleClass().add("MENHIR");
+        ImageView imageMenhir = new ImageView(new Image("/256/MENHIR.jpg"));
         imageMenhir.setId("MENHIR");
         imageMenhir.setFitWidth(ImageLoader.NORMAL_TILE_FIT_SIZE);
         imageMenhir.setFitHeight(ImageLoader.NORMAL_TILE_FIT_SIZE);
@@ -66,7 +62,6 @@ public class DecksUI {
 
         //Current Tile
         StackPane currentTile = new StackPane();
-        currentTile.getStyleClass().add("next-tile");
         currentTile.setId("next-tile");
         box.getChildren().add(currentTile);
 
@@ -84,8 +79,6 @@ public class DecksUI {
 
 
         Text textCurrent = new Text();
-        textCurrent.getStyleClass().add("next-tile");
-        textCurrent.setId("next-tile");
         textCurrent.textProperty().bind(text);
         textCurrent.visibleProperty().bind(text.map(txt -> !txt.isEmpty()));
         textCurrent.setOnMouseClicked(e -> event.accept(null));
