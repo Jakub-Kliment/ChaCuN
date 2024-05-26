51a0aafc4dfd92852582f815673d532c414d6fda
Jakub Kliment <jakub.kliment@epfl.ch>
2024-05-27 01:22:21 +0200

    amelioration du code Icon

--- src/ch/epfl/chacun/gui/Icon.java
+++ src/ch/epfl/chacun/gui/Icon.java
@@ -13,6 +13,10 @@ import javafx.scene.shape.SVGPath;
  */
 public final class Icon {
 
+    private final static String PAWN_SVG_PATH = "M -10 10 H -4 L 0 2 L 6 10 H 12 L 5 0 L " +
+            "12 -2 L 12 -4 L 6 -6 L 6 -10 L 0 -10 L -2 -4 L -6 -2 L -8 -10 L -12 -10 L -8 6 Z";
+
+    private final static String HUT_SVG_PATH = "M -8 10 H 8 V 2 H 12 L 0 -10 L -12 2 H -8 Z";
     /**
      * Private constructor that prevents instantiation.
      */
@@ -27,16 +31,11 @@ public final class Icon {
      * @return the new icon of the occupant that belongs to the player
      */
     public static Node newFor(PlayerColor color, Occupant.Kind kind) {
-        SVGPath svg = new SVGPath();
-        if (kind == Occupant.Kind.PAWN)
-            svg.setContent("M -10 10 H -4 L 0 2 L 6 10 H 12 L 5 0 L 12 -2 L 12 -4 L " +
-                    "6 -6 L 6 -10 L 0 -10 L -2 -4 L -6 -2 L -8 -10 L -12 -10 L -8 6 Z");
-        else
-            svg.setContent("M -8 10 H 8 V 2 H 12 L 0 -10 L -12 2 H -8 Z");
-
-        svg.setFill(ColorMap.fillColor(color));
-        svg.setStroke(ColorMap.strokeColor(color));
-        return svg;
+        SVGPath svgPath = new SVGPath();
+        svgPath.setContent(kind == Occupant.Kind.PAWN ? PAWN_SVG_PATH : HUT_SVG_PATH);
+        svgPath.setFill(ColorMap.fillColor(color));
+        svgPath.setStroke(ColorMap.strokeColor(color));
+        return svgPath;
     }
 }
 
